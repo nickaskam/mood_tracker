d60f1796dcd3ff49dc78bc597bf67d7fbafec8a0
Nick Askam <asknickfilms@gmail.com>
2025-01-01 18:57:48 -0700

    Adding the first questions

--- question.py
+++ question.py
@@ -1,5 +1,14 @@
-from question import Question
 
+class Question:
+    def __init__(self, question_text: str):
+        self.question_text = question_text
+
+    def return_question_text(self):
+        return self.question_text
+    
+    def change_question_text(self, new_question_text: str):
+        self.question_text = new_question_text
+    
 if __name__ == '__main__':
     question_to_ask = 'How are you?'
     new_question = Question(question_to_ask)
