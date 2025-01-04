17a6de2e67abcbd817f068bf4ec20b72cce410a1
Nick Askam <asknickfilms@gmail.com>
2025-01-04 15:02:42 -0700

    moving latest tests into main

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 from question import Question
 from questions_list import Question_List
+from response import Response
 
 if __name__ == '__main__':
-    question1_text = "What is your mood?"
-    question1 = Question(question1_text)
+    question_text = "What is your mood today?"
+    question = Question(question_text)
 
-    question2_text = "Do you feel accomplished today?"
-    question2 = Question(question2_text)
-
-    question_list = [question1, question2]
+    question_list = [question]
     new_question_list = Question_List(question_list)
+    
+    response_text = "5 - High"
+    response_test = Response(question.return_question_id(), response_text)
+    print("Response ID:", response_test.return_response_id())
+    print("Response Time:", response_test.return_response_time())
+    question_id_in_response = response_test.return_question_id()
+    print("Response Question ID:", question_id_in_response)
+    print("Response Text:", response_test.return_response_text())
 
-    print("Questions Asked:")
-    new_question_list.print_questions()
-    print("Specific Question:")
-    print(new_question_list.return_one_question(1))
-
-    question2_updated_text = "Did you get a lot done today?"
-    new_question_list.change_question(1, question2_updated_text)
-    print("Updated question 2 text:")
-    new_question_list.print_questions()
-
-    print("What are the IDs for the questions: ", new_question_list.return_question_ids())
+    retrieved_question_text = new_question_list.return_question_text_from_uuid(question_id_in_response)
+    print("Question Text:", retrieved_question_text)
