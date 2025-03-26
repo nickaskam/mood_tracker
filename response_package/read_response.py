7146e01fa7a5bb81981e7588b7283440c3b4aa33
Nick Askam <asknickfilms@gmail.com>
2025-03-25 22:05:24 -0600

    using class based component for read response

--- response_package/read_response.py
+++ response_package/read_response.py
@@ -1,16 +1,30 @@
 import csv
 
-response_requested = input("Enter a response ID that you want: ")
+CSV_LOCATION = '/Users/nickaskam/Documents/2025/mood_tracker/responses.csv'
 
 # For testing only, look for response regardless of what the user requested
 response_requested = '8968a4bb-b00f-4417-82d2-6a80ce5fe0e2'
 
-with open('/Users/nickaskam/Documents/2025/mood_tracker/responses.csv', newline='') as csvfile:
-    csv_reader = csv.reader(csvfile)
-    for row in csv_reader:
-        if row[0] == response_requested:
-            print("\nResponse ID: ", row[0])
-            print("Response Time: ", row[1])
-            print("Question ID: ", row[2])
-            print("Reponse Text: ", row[3])
-            print("User ID: ", row[4])
+class Read_Response:
+    def __init__(self, csv_file_location):
+        self.csv_file_location = csv_file_location
+
+    def read_response(self, response_requested):
+        with open('/Users/nickaskam/Documents/2025/mood_tracker/responses.csv', newline='') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            for row in csv_reader:
+                if row[0] == response_requested:
+                    print("\nResponse ID: ", row[0])
+                    print("Response Time: ", row[1])
+                    print("Question ID: ", row[2])
+                    print("Reponse Text: ", row[3])
+                    print("User ID: ", row[4])
+
+
+if __name__ == "__main__":
+    # For testing only, look for response regardless of what the user requested
+    response_requested = '8968a4bb-b00f-4417-82d2-6a80ce5fe0e2'
+
+    new_reader = Read_Response(CSV_LOCATION)
+    new_reader.read_response(response_requested)
+    
